9f11e351a61686900136a05ce9737df97c989051
cap9qd <33727568+cap9qd@users.noreply.github.com>
2023-12-09 18:42:46 -0600

    Updates for new BK7231T based GoSund SW2...
    Changed to a UARTDevice (instead of MQTT), removed MQTT functions. The new SW2 has a BK7231T chip and a different dimmer module with different serial interface.

--- devices/gosund_sw2_light.cpp
+++ devices/gosund_sw2_light.cpp
@@ -1,116 +1,93 @@
+//Used reverse engineering info from: https://www.elektroda.com/rtvforum/topic4014774.html#20825305
+
 #include "gosund_sw2_light.hpp"
 
-namespace esphome {
-  namespace gosund {
-    static const char *TAG = "gosund.light.sw2";
-    static const byte ON_MASK = 0x80;
+namespace esphome
+{
+    namespace gosund
+    {
+        static const char *TAG = "gosund.light.sw2";
+        static const byte ON_MASK = 0x80;
 
-    static const uint8_t MIN_VALUE = 0;
-    static const uint8_t MAX_VALUE = 100;
-    static const uint8_t MIN_PERCENT = 1;
+        static const uint8_t MIN_VALUE = 0;
+        static const uint8_t MAX_VALUE = 100;
+        static const uint8_t MIN_PERCENT = 1;
+        
+        void GosundLight::setup() {}
 
-    void GosundLight::populateBrightnessTopic(char * topic, int len) {
-      snprintf(topic, len, "%s/light/%s/command/brightness_pct",
-	       mqtt::global_mqtt_client->get_topic_prefix().c_str(),
-	       state_->get_object_id().c_str());
-    }      
+        void GosundLight::loop()
+        {
+            // pattern is 0x24 0xYY 0x01 0x1E 0x23, where YY is dimmer value
+            if (available() >= 5)
+            {
+                uint8_t buff[5];
 
-    void GosundLight::setup() {
-      char topic[256];
-      populateBrightnessTopic(topic, sizeof(topic));
-      subscribe(topic, &GosundLight::on_brightness_pct_message);
+                //
+                read_array(&buff[0], 5);
+                
+                if (available() > 0)
+                {
+                    ESP_LOGD(TAG, "Unexpected bytes on uart_. ");
+                    ESP_LOGD(TAG, "  Expected: %02X %02X %02X %02X %02X", buff[0], buff[1], buff[2], buff[3], buff[4]);
+                    ESP_LOGD(TAG, "  Expected: %02X %02X %02X %02X %02X", buff[0], buff[1], buff[2], buff[3], buff[4]);
+
+                    while (available() >= 1)
+                    {
+                        read_byte(&buff[0]);
+                        ESP_LOGD(TAG, "  Unexpected: %02X", buff[0]);
+                    }
+                }
+
+                float dimmerVal = buff[1] / 150.0; //Brightness returned is 0x01 - 0x96
+
+                auto call = state_->make_call();
+                // Touch sensor only works when turned on
+                call.set_state(true);
+                call.set_brightness(dimmerVal);
+                call.perform();
+
+                ESP_LOGD(TAG, "Received dimmer value %02X %d", buff[1], buff[1]);
+            }
+        }
       
-      ESP_LOGD(TAG, "Subscribed to: %s", topic);
-    }
-
-    void GosundLight::loop() {
-      // pattern is 0x24 0xYY 0x01 0x1E 0x23, where YY is dimmer value
-      if (Serial.available() >= 5) {
-	char buff[5];
-
-	//
-	while (Serial.available() >= 5) {
-	  Serial.readBytes(buff, 5);
-	}
-	if (Serial.available() > 0) {
-	  ESP_LOGD(TAG, "Unexpected bytes on serial. ");
-	  ESP_LOGD(TAG, "  Expected: %02X %02X %02X %02X %02X",
-		   buff[0], buff[1], buff[2], buff[3], buff[4]);
-	  
-	  while (Serial.available() >= 1) {
-	    Serial.readBytes(buff, 1);
-	    ESP_LOGD(TAG, "  Unexpected: %02X", buff[0]);
-	  }
-	}
-
-	float dimmerVal = .01 * buff[1];
-
-	set_by_touch_ = true;    /* Prevent sending the value back to the hardware */
-	auto call = state_->make_call();
-	// Touch sensor only works when turned on
-	call.set_state(true);
-	call.set_brightness(dimmerVal);
-	call.perform();
-      
-	ESP_LOGD(TAG, "Received dimmer value %02X %d", buff[1], buff[1]);
-      }
-    }
+        light::LightTraits GosundLight::get_traits()
+        {
+            auto traits = light::LightTraits();
+            traits.set_supported_color_modes({light::ColorMode::BRIGHTNESS});
+            return traits;
+        }
   
-    void GosundLight::dump_config() {
-      char topic[256];
-      populateBrightnessTopic(topic, sizeof(topic));
+        void GosundLight::write_state(light::LightState *state)
+        {
+            auto values = state->current_values;
+            uint8_t output = std::min(MAX_VALUE, (uint8_t) (100.0 * values.get_brightness()));
+            output = std::max(MIN_PERCENT, output);
+            
+            uint8_t ledOut = std::max(MIN_PERCENT, (uint8_t) (values.get_brightness() * 7.0));
+            
+            if (values.get_state() > 0 && values.get_brightness() > 0)
+            {
+                status_led_->turn_on();
+                ESP_LOGD(TAG, "turning on status LED");
+                
+                ledOut |= 0x40; //Indicates LED message
+                output |= 0x80; //Indicates output message
+                
+                write_byte(ledOut);
+                write_byte(output);
+            }
+            else
+            {
+                status_led_->turn_off();
+                ESP_LOGD(TAG, "turning off status LED");
+                
+                //write(0x41);
+                write(0x01); //Tell it how many LED to keep on during off
+            }
 
-      ESP_LOGCONFIG(TAG, "Gosund SW2 Dimmer:");
-      ESP_LOGCONFIG(TAG, " Brightness Percent Topic: %s", topic);
-    }
-  
-    light::LightTraits GosundLight::get_traits() {
-      auto traits = light::LightTraits();
-      traits.set_supported_color_modes({light::ColorMode::BRIGHTNESS});
-      return traits;    
-    }
-  
-    void GosundLight::write_state(light::LightState *state) {
-      // don't go into an infinite loop when set by touch
-      if (set_by_touch_) {
-	set_by_touch_ = false;
-	return;
-      }
-    
-      auto values = state->current_values;
-      uint8_t output = std::min(MAX_VALUE,
-				(uint8_t) (100 * values.get_brightness()));
-      output = std::max(MIN_VALUE, output);
-    
-      if (values.get_state() > 0 && values.get_brightness() > 0) {
-	output += ON_MASK;
-	status_led_->turn_on();
-	ESP_LOGD(TAG, "turning on status LED");
-      } else {
-	status_led_->turn_off();
-	ESP_LOGD(TAG, "turning off status LED");
-      }
-
-      ESP_LOGD(TAG, "write_state() called with state: %0.1f, brightness: %.02f => output: %02X",
-	       values.get_state(), values.get_brightness(), output);
-
-      Serial.write(output);
-    }  
-
-    void GosundLight::on_brightness_pct_message(const std::string &payload) {
-      uint8_t brightness = atoi(payload.c_str());
-
-      ESP_LOGD(TAG, "Received MQTT Brightness request for %d", brightness);
-      
-      brightness = std::max(MIN_PERCENT, brightness);
-      brightness = std::min(MAX_VALUE, brightness);
-
-      float brightnessFloat = 0.01 * brightness;
-
-      auto call = state_->make_call();
-      call.set_brightness(brightnessFloat);
-      set_by_touch_ = false;
-      call.perform();
-    }
-  } // namespace gosund
+            ESP_LOGD(TAG, "write_state() called with state: %0.1f, brightness: %.02f => output: %02X and LED: %02X", values.get_state(), values.get_brightness(), output, ledOut);
+            
+            flush();
+        }
+    } // namespace gosund
 } // namespace esphome
--- devices/gosund_sw2_light.hpp
+++ devices/gosund_sw2_light.hpp
@@ -1,28 +1,27 @@
 #pragma once
 #include "esphome.h"
 
-namespace esphome {
-  namespace gosund {
+namespace esphome
+{
+    namespace gosund
+    {
+        class GosundLight : public Component, public light::LightOutput, public uart::UARTDevice {
 
-    class GosundLight : public Component, public light::LightOutput, public mqtt::CustomMQTTDevice {
-    public:
-      GosundLight(output::BinaryOutput *status_led) { status_led_ = status_led; }
-      void setup() override;
-      void loop() override;
-      void dump_config() override;
-      light::LightTraits get_traits() override;
-      void setup_state(light::LightState *state) override { state_ = state; }
-      void write_state(light::LightState *state) override;
+            public:
+                GosundLight(output::BinaryOutput *status_led, uart::UARTComponent *uart) : UARTDevice(uart)
+                { 
+                    status_led_ = status_led;
+                }
+                void setup() override;
+                void loop() override;
+                light::LightTraits get_traits() override;
+                void setup_state(light::LightState *state) override { state_ = state; }
+                void write_state(light::LightState *state) override;
 
-    protected:
-      bool set_by_touch_ = false;
-      light::LightState *state_{nullptr};
-      output::BinaryOutput *status_led_;
-    
-    private:
-      void on_brightness_pct_message(const std::string &payload);
-      void populateBrightnessTopic(char * topic, int len);
-    };
+            protected:
+                light::LightState *state_{nullptr};
+                output::BinaryOutput *status_led_;
+        };
 
-  } // namespace gosund
+    } // namespace gosund
 } // namespace esphome

67958aa2c1aec01b096c863558304c548cc8dbc6
cap9qd <33727568+cap9qd@users.noreply.github.com>
2023-12-11 21:34:32 -0600

    Fixed a bunch of issues with touch detection.

--- devices/gosund_sw2_light.cpp
+++ devices/gosund_sw2_light.cpp
@@ -1,3 +1,5 @@
+//Forked orginal code from https://github.com/a-lost-shadow/gosund-sw2 for MCU v1
+// 
 //For the BK7231T version (mcu v2?) I used reverse engineering info from: 
 //   https://www.elektroda.com/rtvforum/topic4014774.html#20825305
 
@@ -7,96 +9,93 @@ namespace esphome
 {
     namespace gosund
     {
-        static const char *TAG = "gosund.light.sw2";
-        static const byte ON_MASK = 0x80;
-
-        static const uint8_t MIN_VALUE = 0;
-        static const uint8_t MAX_VALUE = 100;
-        static const uint8_t MIN_PERCENT = 1;
-        
         void GosundLight::setup() {}
+
+        void GosundLight::dump_config() 
+        {
+            ESP_LOGCONFIG(TAG,     "Setup GoSund Dimmer SW2");
+            ESP_LOGCONFIG(TAG,     "\tMCU:           v%d",    mcuVer);
+            ESP_LOGCONFIG(TAG,     "\tDebug:         %d",     debugPrint);
+            
+            //Error message if the MCU version not in [1,2]
+            if(setupError)
+                ESP_LOGE(TAG, "\tERROR SETTING UP MCU VERSION! Please check config!");
+        }
 
         void GosundLight::loop()
         {
             static uint8_t pos = 0;
-            static uint8_t setupSync = 0;
-            static uint8_t mPos = 16;
-            static uint8_t tBuffer[16];
+            static uint8_t mPos = 5;
+            static uint8_t tBuffer[5];
             static float   dimmerVal = 0.0;
             
-            if(available() > 0)
+            if(available())
             {
-                if(pos < mPos - 2)
-                {    
-                    read_byte(&tBuffer[pos++]);
-                    if(debugPrint)
-                        ESP_LOGD(TAG, "READ BYTE! 0x%02X", tBuffer[pos-1]);
-                }
-                else
-                {
-                    pos = mPos - 2;
-                    for(int i = 0;i<mPos-2;i++)
-                        tBuffer[i] = tBuffer[i+1];
-                    read_byte(&tBuffer[pos]);
-                    
-                    if(debugPrint && (tBuffer[0] == 0x24))
-                        ESP_LOGD(TAG, "READ BYTE! 0x%02X %02X %02X %02X %02X", tBuffer[0], tBuffer[1], tBuffer[2], tBuffer[3], tBuffer[4]);
-                }
-                tBuffer[pos+1] = '\0';
-                
+                for(int i = 0;i<4;i++)
+                    tBuffer[i] = tBuffer[i+1];
+                read_byte(&tBuffer[4]);
+            
                 //MCU v1 = 0x24 0xYY 0x01 0x1E 0x23 where 0xYY is the dimmer value
                 //MCU v2 = 0x24 0xYY 0x01 0x64 0x23 where 0xYY is the dimmer value
-                if(tBuffer[0] == 0x24)
+                // (NOTE: v1 MCU swapped byte 3 around between reboots; idk why...)
+                if((tBuffer[0] == 0x24) && (0x01 == tBuffer[2]) && (0x23 == tBuffer[4]))
                 {
-                    uint32_t tCompare = (tBuffer[0] << 24 ) \
-                                      + (tBuffer[2] << 16 ) \
-                                      + (tBuffer[3] << 8 ) \
-                                      +  tBuffer[4];
-                    
-                    if(printDebug)
-                        ESP_LOGD(TAG, "Dimmer Compare 0x%08X", tCompare);
-                    
-                    if(tCompare == touchCompare)
+                    if(debugPrint)
                     {
-                        dimmerVal = tBuffer[1] / 150.0; //Brightness returned is 0x01 - 0x96
-                        setupSync = 1;
-                        memset(&tBuffer[0], 0, 5);
-                        if(debugPrint)
-                            ESP_LOGD(TAG, "Received dimmer value: %01.1f", dimmerVal*100);
-                    
-                        auto call = state_->make_call();
-                        // Touch sensor only works when turned on
-                        call.set_state(true);
-                        call.set_brightness(dimmerVal);
-                        call.perform();
+                        ESP_LOGD(TAG, "READ BYTES! 0x%02X %02X %02X %02X %02X", \
+                                tBuffer[0], tBuffer[1], tBuffer[2], tBuffer[3], tBuffer[4]);
                     }
+                    
+                    //Brightness returned is 0x01 - 0x64 for v1
+                    //Brightness returned is 0x01 - 0x96 for v2
+                    dimmerVal = tBuffer[1] / MAX_VALUE; 
+                    
+                    //Clear buffer so we dont trigger more than 1 time per message.
+                    memset(&tBuffer[0], 0, 5); 
+                    
+                    if(debugPrint)
+                    {
+                        ESP_LOGD(TAG, "Received dimmer value: %01.1f", dimmerVal*100);
+                    }
+                    
+                    auto call = state_->make_call();
+                    // Touch sensor only works when turned on
+                    call.set_state(true);
+                    call.set_brightness(dimmerVal);
+                    call.perform();
                 }
             }
         }
-      
+
         light::LightTraits GosundLight::get_traits()
         {
             auto traits = light::LightTraits();
             traits.set_supported_color_modes({light::ColorMode::BRIGHTNESS});
             return traits;
         }
-  
+
         void GosundLight::write_state(light::LightState *state)
         {
             auto values = state->current_values;
-            uint8_t output = std::min(MAX_VALUE, (uint8_t) (100.0 * values.get_brightness()));
+            
+            uint8_t output = std::min(MAX_PERCENT, (uint8_t) (100.0 * values.get_brightness()));
             output = std::max(MIN_PERCENT, output);
             
-            uint8_t ledOut = std::max(MIN_PERCENT, (uint8_t) (values.get_brightness() * 7.0));
+            uint8_t ledOut = std::max(MIN_PERCENT, (uint8_t) std::ceil(values.get_brightness() * 7.0));
             
             if (values.get_state() > 0 && values.get_brightness() > 0)
             {
                 status_led_->turn_on();
+
                 if(debugPrint)
+                {
                     ESP_LOGD(TAG, "turning on status LED");
+                }
                 
-                output |= 0x80; //Indicates output message
+                // Last bit indicates ON/OFF state; last bits 0-100% level.
+                output |= 0x80; //Indicates output message and on
                 
+                //Only write LED command is we are MCU v2 and not "byTouch" to maintain MQTT version functionality.
                 if(mcuVer == 2)
                 {
                     ledOut |= 0x40; //Indicates LED message
@@ -107,11 +106,21 @@ namespace esphome
             else
             {
                 status_led_->turn_off();
-                if(debugPrint)
-                    ESP_LOGD(TAG, "turning off status LED");
                 
-                //write(0x41);
-                write(0x01); //Tell it how many LED to keep on during off
+                if(debugPrint)
+                {
+                    ESP_LOGD(TAG, "turning off status LED");
+                }
+                
+                switch(mcuVer)
+                {
+                    case 1:
+                        write(output & 0x7F); //Tell it which level LED to keep on during off
+                        break;
+                    case 2:
+                        write(ledOut & 0x07); //Tell it which level LED to keep on during off
+                        break;
+                }
             }
             if(debugPrint)
                 if(mcuVer == 2)
--- devices/gosund_sw2_light.hpp
+++ devices/gosund_sw2_light.hpp
@@ -8,28 +8,49 @@ namespace esphome
         class GosundLight : public Component, public light::LightOutput, public uart::UARTDevice {
 
             public:
-                GosundLight(output::BinaryOutput *status_led, uart::UARTComponent *uart, int _mcuVer = 2, bool _debugPrint = false) : UARTDevice(uart)
+                GosundLight(output::BinaryOutput *status_led, uart::UARTComponent *uart, uint8_t _mcuVer = 2, bool _debugPrint = false) : UARTDevice(uart)
                 { 
                     status_led_ = status_led;
                     mcuVer = _mcuVer;
                     debugPrint = _debugPrint;
-                    if(mcuVer > 1)
-                        touchCompare = 0x24016423;
+                    
+                    // Set compare; only check last 3 elements later and use first as "msg ready" indication...
+                    switch(mcuVer)
+                    {
+                        case 0x01:
+                            MAX_VALUE = 100.0;
+                            break;
+                        case 0x02:
+                            MAX_VALUE = 150.0;
+                            break;
+                        default:
+                            setupError = true;
+                            break;
+                    }
                 }
+                
                 void setup() override;
                 void loop() override;
                 light::LightTraits get_traits() override;
                 void setup_state(light::LightState *state) override { state_ = state; }
                 void write_state(light::LightState *state) override;
+                void dump_config();
 
             protected:
                 light::LightState *state_{nullptr};
                 output::BinaryOutput *status_led_;
-                int mcuVer = 0;
+                uint8_t mcuVer = 0;
                 bool debugPrint = false;
-                //MCU v1 = 0x24 0xYY 0x01 0x1E 0x23 where 0xYY is the dimmer value
-                //MCU v2 = 0x24 0xYY 0x01 0x64 0x23 where 0xYY is the dimmer value
-                uint32_t touchCompare = 0x24011E23;
+                
+                const char *TAG = "gosund.light.sw2";
+                const byte ON_MASK = 0x80;
+
+                const uint8_t MAX_PERCENT = 100;
+                const uint8_t MIN_PERCENT = 1;
+
+                float MAX_VALUE = 100.0;
+
+                bool setupError = 0;
         };
 
     } // namespace gosund
